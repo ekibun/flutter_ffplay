331a03ef7ea9b7f3f82ea6f502078b13b4329d03
ekibun <soekibun@gmail.com>
2021-06-28 16:45:08 +0800

    init

---
+++ example/windows/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,12 @@
+//
+//  Generated file. Do not edit.
+//
+
+#include "generated_plugin_registrant.h"
+
+#include <player/player_plugin.h>
+
+void RegisterPlugins(flutter::PluginRegistry* registry) {
+  PlayerPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("PlayerPlugin"));
+}
